6a1c230de8959bfbe2d92aa40e805b85a42fb6d1
Fukuwa <fjoriofi@gmail.com>
2018-06-14 20:42:06 +0300

    Added Events.ejs
    Updated app.js with a neq route and events data;
    Updated index.ejs with nav items (Link and Text).

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 // System specified port
 //var port = process.env.port;
 var port = 5000;
+var eventRouter = express.Router();
 
 // Static folder locations
 app.use(express.static('public'));
@@ -14,12 +15,107 @@ app.use(express.static('bower_components'));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
 
+// Event data to be tracked
+var eventsData = [
+    {
+        name: 'Event 1',
+        description: 'The first event',
+        date: '2016.01.01',
+        time: '1:00 午後',
+        duration: '1 Hour',
+        location: {
+            streetAddr: '101 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 100
+    },
+    {
+        name: 'Event 2',
+        description: 'The second event',
+        date: '2016.02.02',
+        time: '2:00 午後',
+        duration: '2 Hours',
+        location: {
+            streetAddr: '202 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 200
+    },
+    {
+        name: 'Event 3',
+        description: 'The third event',
+        date: '2016.03.03',
+        time: '3:00 午後',
+        duration: '3 Hour',
+        location: {
+            streetAddr: '303 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 300
+    },
+    {
+        name: 'Event 4',
+        description: 'The fourth event',
+        date: '2016.04.04',
+        time: '4:00 午後',
+        duration: '4 Hour',
+        location: {
+            streetAddr: '404 Main St.',
+            city: 'Los Angeles',
+            state: 'CA',
+            zip: '87885',
+            lon: 0,
+            lat: 0
+        },
+        capacity: 400
+    }
+
+];
+
+/**
+ * Renders events page
+ * Passes in list, nav and events
+ */
+eventRouter.route('/')
+    .get(function(req, res){
+        res.render('events', {
+            list: ['1st Event', '2nd Event', '3rd Event'],
+            nav: [{ Link: 'Services', Text: 'Services' },
+                { Link: 'Portfolio', Text: 'Portfolio' },
+                { Link: 'About', Text: 'About' },
+                { Link: 'Team', Text: 'Team' },
+                { Link: 'Contact', Text: 'Contact' },
+                { Link: 'Events', Text: 'Events' }
+            ],
+            events: eventsData
+        });
+    });
+    // States that the route is used
+app.use('/Events', eventRouter);
+
 // Specifies request type
 app.get('/', function(req, res){
     //res.send('App message');
     res.render('index', {
         list: ['1st val', '2nd val', '3rd val'],
-        nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact']
+        nav: [{ Link: 'Services', Text: 'Services' },
+            { Link: 'Portfolio', Text: 'Portfolio' },
+            { Link: 'About', Text: 'About' },
+            { Link: 'Team', Text: 'Team' },
+            { Link: 'Contact', Text: 'Contact' },
+            { Link: 'Events', Text: 'Events' }]
     });
 });
 

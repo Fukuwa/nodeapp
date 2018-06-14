8e73705d6e9629949a84902b589e11f9cc2bfe9b
Fukuwa <fjoriofi@gmail.com>
2018-06-14 03:06:43 +0300

    Updated gulpfile and bower.json
    Added overrides to bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,25 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 
-// Looks for files from project root and 'src' folder
+// Looks for js files from project root and 'src' folder
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function(){
     return gulp.src(jsFiles)
         .pipe(jshint());
+});
+
+gulp.task('inject', function(){
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './bower_components',
+        // Shortens the path in index.html file
+        ignorePath: '../../bower_components'
+    };
+
+    // Adds html files location for gulp
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
 });
